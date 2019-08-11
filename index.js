7beb0562953af70363c91a2d306f636153587ff6
boomperdack <boomperdack@gmail.com>
2019-08-11 09:35:41 +0300

    some js 11.08

---
+++ index.js
@@ -0,0 +1,29 @@
+// // сперд оператор
+
+// const obj1 = { foo: 'bar', samekey: '1'};
+// const obj2 = { foo: 'bar2' ,samekey:'2'};
+// const obj3 = {
+//     samekey: '3',
+//     ...obj1,
+//     ...obj2,
+//     customkey : 'customValue',
+// };
+// //console.log(obj3);
+
+// //
+// const { samekey } = obj3;
+// const nextObj3 = {...obj3};
+// delete nextObj3.samekey;
+
+// const[first, second, ...nextArr] = [1,2,3,4,5,6,7];
+
+// //console.log(first, second, ...nextArr);
+
+// function helloUser ({firstName , lastName}) {
+//    // console.log ('Hello' + ' ' + firstName + ' ' + lastName + '!');
+// }
+// helloUser({
+//     firstName: 'Max',
+//     lastName: 'Demidov',
+// });
+
