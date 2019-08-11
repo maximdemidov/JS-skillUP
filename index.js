b2b40ee3becda6f3c8afabebf6f8693ab110d069
boomperdack <boomperdack@gmail.com>
2019-08-11 10:16:37 +0300

    some fuction and check test

--- index.js
+++ index.js
@@ -26,4 +26,50 @@
 //     firstName: 'Max',
 //     lastName: 'Demidov',
 // });
+const a = 'lolkek'
+const obj = {
+    a,
+};
 
+function getUserInput(type, inputFunc = prompt) {
+    const input = inputFunc();
+
+    if (input === null) {
+        return null;
+    }
+
+    switch (type) {
+        case 'string':
+            return input;
+        case 'number':
+            if (input === '') {
+                return null;
+            }
+            const number = +input;
+            return isNaN(number) ? null : number;
+    }
+
+    return null;
+}
+console.log(getUserInput('string'));
+console.log(getUserInput('number'));
+
+// test
+function test(func, inputArgs = [], expectedResult) {
+    const result = func(...inputArgs);
+
+    console.log ('--------/n')
+
+    if (result === expectedResult) {
+        console.log('%c Test pass!', 'color: #45A163');
+        return true;
+    }
+    console.log('%c Test fail!', 'color: red');
+    console.log({func, inputArg, result, expectedResult});
+}
+
+test(getUserInput, ['number', () => 123], 123);
+test(getUserInput, ['number', () => null], null);
+test(getUserInput, ['number', () => '345'], 345);
+test(getUserInput, ['number', () => 'asdfg'], null);
+test(getUserInput, ['number', () => ''], null);
