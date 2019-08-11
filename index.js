889d7408d3e52d761969e630431f98a126f41ffc
boomperdack <boomperdack@gmail.com>
2019-08-11 10:51:17 +0300

    add triangle and check test not finished

--- index.js
+++ index.js
@@ -26,6 +26,9 @@
 //     firstName: 'Max',
 //     lastName: 'Demidov',
 // });
+
+
+
 const a = 'lolkek'
 const obj = {
     a,
@@ -51,25 +54,56 @@ function getUserInput(type, inputFunc = prompt) {
 
     return null;
 }
-console.log(getUserInput('string'));
-console.log(getUserInput('number'));
+// console.log(getUserInput('string'));
+// console.log(getUserInput('number'));
 
 // test
 function test(func, inputArgs = [], expectedResult) {
     const result = func(...inputArgs);
 
-    console.log ('--------/n')
+    // console.log ('--------/n')
 
     if (result === expectedResult) {
-        console.log('%c Test pass!', 'color: #45A163');
+        // console.log('%c Test pass!', 'color: #45A163');
         return true;
     }
-    console.log('%c Test fail!', 'color: red');
-    console.log({func, inputArg, result, expectedResult});
+    // console.log('%c Test fail!', 'color: red');
+    // console.log({func, inputArg, result, expectedResult});
 }
 
-test(getUserInput, ['number', () => 123], 123);
-test(getUserInput, ['number', () => null], null);
-test(getUserInput, ['number', () => '345'], 345);
-test(getUserInput, ['number', () => 'asdfg'], null);
-test(getUserInput, ['number', () => ''], null);
+// test(getUserInput, ['number', () => 123], 123);
+// test(getUserInput, ['number', () => null], null);
+// test(getUserInput, ['number', () => '345'], 345);
+// test(getUserInput, ['number', () => 'asdfg'], null);
+// test(getUserInput, ['number', () => ''], null);
+
+function checkIfNumber(input) {
+    return typeof input === 'number' && !isNaN(input);
+}
+
+function checkIfTriangle(a = 0, b = 0 , c = 0) {
+
+    if(![a,b,c].every(checkIfNumber)) {
+        return false;
+    }
+    
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        return false;
+    } 
+    return true;
+}
+
+function getTriangleSquare(a = 0, b = 0 , c = 0) {
+    if (!checkIfTriangle(a, b, c)) {
+        return null;
+    }
+
+    const p = (a + b + c) / 2;
+    const square = Math.sqrt (
+        p * (p - a) * (p - b) * (p - c)
+    );
+    return square;
+}
+test(getTriangleSquare, [3 , 4 , 5], 6);
+test(getTriangleSquare, [1 , 2 , 3], null);
+test(getTriangleSquare, [-3 , -4 , -5], null);
